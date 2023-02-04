03512716dd3c1045606d0ca5cfacaa26e79cdcc2
mbrenn <mbrenn@vt.edu>
2023-02-04 18:33:59 -0500

    partially working

--- auditory_assistant/src/trackingLogic/tracking.js
+++ auditory_assistant/src/trackingLogic/tracking.js
@@ -2,24 +2,74 @@
 import * as handpose from "@tensorflow-models/handpose";
 import "@tensorflow/tfjs-backend-webgl";
 import { useEffect } from "react";
+import { Camera } from "@mediapipe/camera_utils";
+import { } from "@mediapipe/control_utils";
+import {drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { Hands, HAND_CONNECTIONS } from "@mediapipe/hands";
 
 const Tracking = () => {
-  const tracking = async () => {
-    // Load the MediaPipe handpose model assets.
-    
-    const model = await handpose.load();
-    // Pass in a video stream to the model to obtain
-    // a prediction from the MediaPipe graph.
-    const video = document.querySelector("#videoElement");
-    const hands = await model.estimateHands(video);
-    console.log(hands);
-    // Each hand object contains a `landmarks` property,
-    // which is an array of 21 3-D landmarks.
-    hands.forEach((hand) => console.log(hand.landmarks));
-  };
+  const videoElement = document.getElementById("input_video");
+  const canvasElement = document.getElementById("output_canvas");
+  const canvasCtx = canvasElement.getContext("2d");
+
+  const onResults = (results) => {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5,
+        });
+        drawLandmarks(canvasCtx, landmarks, {
+          color: "#FF0000",
+          lineWidth: 2,
+        });
+        console.log(results.multiHandLandmarks);
+      }
+    }
+    canvasCtx.restore();
+  }
+
+  const startCamera = async () => {
+    console.log("camera started");
+    const hands = new Hands({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      },
+    });
+  
+    hands.setOptions({
+      maxNumHands: 2,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
+  
+    hands.onResults(onResults);
+  
+    const camera = new Camera(videoElement, {
+      onFrame: async () => {
+        await hands.send({ image: videoElement });
+      },
+      width: 1280,
+      height: 720,
+    });
+    console.log("right before start");
+    await camera.start();
+    console.log("right after start");
+  }
 
   useEffect(() => {
-    tracking();
+    console.log("waddup")
+    startCamera();
   });
 
   return null;
