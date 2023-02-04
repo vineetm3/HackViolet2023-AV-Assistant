0b2aa843894148fe3dac76e83190ad82235db738
AndrewTran03 <andrewtranva@gmail.com>
2023-02-04 17:44:35 -0500

    02/04/23: Fixed Window to Reasonable Size, Added Logic to Display Resulting Image and Description After Image Analysis Algorithm is Run, Added Title/Header, and CSS Padding

--- auditory_assistant/src/App.js
+++ auditory_assistant/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { gestureMap, gestureMapKey } from './trackingLogic/gestures';
 
 function App() {
+  console.log("TEST: ");
+  console.log(gestureMapKey);
+  const gestureImgLink = gestureMap.get(gestureMapKey)[0]; //Determine which image to display
+  const gestureImgAltText = gestureMap.get(gestureMapKey)[1]; //Determine what alt text to display if image fails
+  const gestureImgCommand = gestureMap.get(gestureMapKey)[2];
+  console.log(gestureImgLink);
+  console.log(gestureImgAltText);
+  console.log(gestureImgCommand);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-Header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Auditory-Assistant Google Extension!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-Gesture-Img-and-Description">
+        <img src={gestureImgLink} className="App-Gesture-Img" alt={gestureImgAltText} />
+        <p>
+          {gestureImgCommand}
+        </p>
+      </div>
     </div>
   );
 }
