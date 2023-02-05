9feb867a08b0f8c1c87ff40f8f7553fb0fb35da8
AndrewTran03 <andrewtranva@gmail.com>
2023-02-04 23:00:54 -0500

    02/04/23: Removed Unused Image Import

--- auditory_assistant/src/App.js
+++ auditory_assistant/src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import './App.css';
 import { gestureMap, gestureMapKey } from './trackingLogic/gestures.js';
 import { GetTextFromHTML } from "./trackingLogic/extension.js";
